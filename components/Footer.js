19552c82bf373b032729aa9bcacdbfee3a93d081
AkshayBenny <akshaybennygh@gmail.com>
2022-10-15 17:39:57 +0530

    basic setup

--- components/Footer.js
+++
@@ -1,5 +0,0 @@
-const Footer = () => {
-  return <div className='w-full bg-black h-12 bottom-0'></div>;
-};
-
-export default Footer;
--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env:{
-    MONGO_URL:"mongodb+srv://akbenn:akbenn@cluster0.rnyfn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-  }
+  swcMinify: true,
 }
 
 module.exports = nextConfig
